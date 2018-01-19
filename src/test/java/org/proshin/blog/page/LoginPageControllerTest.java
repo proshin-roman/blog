5377b68cd84fb19c41d546881b81d1b64fb0a4a6
Roman Proshin <roman@proshin.org>
2018-01-19 23:28:16 +0300

    #21: Improve test coverage >80%

---
+++ src/test/java/org/proshin/blog/page/LoginPageControllerTest.java
@@ -0,0 +1,29 @@
+package org.proshin.blog.page;
+
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.containsString;
+import org.junit.Test;
+import org.proshin.blog.AbstractIntegrationTest;
+import org.proshin.blog.configuration.ConfigParameters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class LoginPageControllerTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private ConfigParameters configParameters;
+
+    @Test
+    public void testLogin() throws Exception {
+        getMvc()
+                .perform(
+                        MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("class=\"g-recaptcha input-group\"")))
+                .andExpect(
+                        content().string(containsString(
+                                format("data-sitekey=\"%s\">", configParameters.getReCaptcha().getKey()))));
+    }
+}
---
+++ src/test/java/org/proshin/blog/page/admin/AdminPagesControllerTest.java
@@ -0,0 +1,22 @@
+package org.proshin.blog.page.admin;
+
+import static org.hamcrest.Matchers.containsString;
+import org.junit.Test;
+import org.proshin.blog.AbstractIntegrationTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WithMockUser(roles = "ADMIN")
+public class AdminPagesControllerTest extends AbstractIntegrationTest {
+
+    @Test
+    public void testGet() throws Exception {
+        getMvc()
+                .perform(
+                        get("/admin"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<a href=\"/admin/posts/\"")));
+    }
+}
